544c8aaa8d44a7b98b1ec8a2cf4acfa703e48b9a
Christopher Fujino <christopherfujino@gmail.com>
2021-12-25 17:52:39 -0800

    add more debug

--- zig/configure-debian.sh
+++ zig/configure-debian.sh
@@ -15,6 +15,9 @@ ZIG_VERSION='0.9.0'
 ZIG_ARCHIVE="zig-linux-x86_64-$ZIG_VERSION.tar.xz"
 curl -lO "https://ziglang.org/download/$ZIG_VERSION/$ZIG_ARCHIVE"
 
+ls -alh "./$ZIG_ARCHIVE"
+file "./$ZIG_ARCHIVE"
+
 tar xvzf "./$ZIG_ARCHIVE"
 
 file "$PWD/zig-linux-x86_64-$ZIG_VERSION/zig"
